b390d4a7ce2aac16a5aa3c56929e14323943ccd0
Jeromeski <jeromegee@gmail.com>
2020-06-13 10:41:32 +0800

    AppBar component

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, useScrollTrigger } from '@material-ui/core';
 
 const Header = () => {
+  function ElevationScroll(props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0
+    });
+
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0
+    });
+  }
   return (
     <React.Fragment>
-      <AppBar position="fixed">
-        <Toolbar> Test </Toolbar>
-      </AppBar>
-      <Toolbar />
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolbar> Test </Toolbar>
+
+          <Toolbar />
+        </AppBar>
+      </ElevationScroll>
     </React.Fragment>
   );
 };
